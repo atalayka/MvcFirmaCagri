ce3fa8e6059be8e35362160d201dc6b5807474a9
AT K <kabakciatalay@gmail.com>
2023-12-21 12:33:19 +0300

    aktif, pasif cagrilar ve cagri detaylari düzenlendi.

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcFirmaCagri.Models.Entity;
 
 namespace MvcFirmaCagri.Controllers
 {
@@ -13,9 +14,42 @@ namespace MvcFirmaCagri.Controllers
         {
             return View();
         }
+        DbIsTakiipEntities db = new DbIsTakiipEntities();
         public ActionResult AktifCagrilar()
+        {
+            var cagrilar = db.TblCagrilar.Where(x=>x.Durum == true).ToList();
+            return View(cagrilar);
+        }
+        public ActionResult PasifCagrilar()
+        {
+            var cagrilar = db.TblCagrilar.Where(x => x.Durum == false).ToList();
+            return View(cagrilar);
+        }
+
+        [HttpGet]
+        public ActionResult YeniCagri()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public ActionResult YeniCagri(TblCagrilar p)
+        {
+            p.Durum = true;
+            p.Tarih = DateTime.Today;
+            p.CagriFirma = 4;
+            db.TblCagrilar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("AktifCagrilar");
+
+        }
+
+        public ActionResult CagriDetay(int id)
+        {
+            var cagri = db.TblCagriDetay.Where(x=>x.Cagri==id).ToList();
+            return View(cagri);
+        }
+
     }
 }
